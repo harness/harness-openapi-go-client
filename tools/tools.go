9e91fbec75adc4a257397ea6b0d8edeab5e2bd16
Richa Jajoo <richa.jajoo@harness.io>
2022-10-20 13:02:53 +0530

    Create tools.go

---
+++ tools/tools.go
@@ -0,0 +1,9 @@
+//go:build tools
+
+package tools
+
+import (
+	_ "github.com/git-chglog/git-chglog/cmd/git-chglog"
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "gotest.tools/gotestsum"
+)
