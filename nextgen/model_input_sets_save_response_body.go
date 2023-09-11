69baca7a655e172d1aefae2af994649da994c975
Ved <ved.atultambat@harness.io>
2023-09-11 15:30:52 +0530

    testing complete

--- nextgen/model_input_sets_save_response_body.go
+++ nextgen/model_input_sets_save_response_body.go
@@ -2,6 +2,6 @@ package nextgen
 
 // Response body for InputSet save.
 type InputSetSaveResponseBody struct {
-	Identifier         string               `json:"identifier,omitempty"`
+	Identifier         string               `json:"input_set_identifier,omitempty"`
 	GovernanceMetadata []GovernanceMetadata `json:"governance_metadata,omitempty"`
 }
